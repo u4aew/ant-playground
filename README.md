0b2b516f9673febfbb1136b2204bc943cb88afbf
sergejucaev <dialoggg1994@gmail.com>
2023-09-12 21:55:18 +0300

    init

--- README.md
+++ README.md
@@ -1,46 +1,55 @@
-# Getting Started with Create React App
+## Введение
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Этот проект создан для экспериментов с библиотекой Ant Design, одним из популярных UI-фреймворков.
 
-## Available Scripts
+## Технологии
 
-In the project directory, you can run:
+- React.js
+- Ant Design
 
-### `npm start`
+## Установка и Запуск
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+1. Клонировать репозиторий:
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+   ```bash
+   git clone <URL_репозитория>
+   ```
 
-### `npm test`
+2. Установить зависимости:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+   ```bash
+   npm install
+   ```
 
-### `npm run build`
+   или
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+   ```bash
+   yarn
+   ```
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+3. Запустить проект:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+   ```bash
+   npm start
+   ```
 
-### `npm run eject`
+   или
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+   ```bash
+   yarn start
+   ```
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Особенности
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+- Интерфейс, построенный на компонентах Ant Design.
+- Примеры использования различных элементов управления и компонентов.
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+## Как Contributing
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+1. Сделать Fork проекта.
+2. Создать новую ветку (`git checkout -b feature/AmazingFeature`).
+3. Произвести изменения в коде.
+4. Добавить изменения (`git add .`).
+5. Сделать коммит (`git commit -m 'Add some AmazingFeature'`).
+6. Произвести Push ветки (`git push origin feature/AmazingFeature`).
+7. Открыть Pull Request.
--- src/src/demo/index.tsx
+++ src/src/demo/index.tsx
@@ -1,61 +1,59 @@
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useState } from 'react';
+import { PoweroffOutlined } from '@ant-design/icons';
+import { Button, Space } from 'antd';
 
-const onFinish = (values: any) => {
-    console.log('Success:', values);
+const App: React.FC = () => {
+    const [loadings, setLoadings] = useState<boolean[]>([]);
+
+    const enterLoading = (index: number) => {
+        setLoadings((prevLoadings) => {
+            const newLoadings = [...prevLoadings];
+            newLoadings[index] = true;
+            return newLoadings;
+        });
+
+        setTimeout(() => {
+            setLoadings((prevLoadings) => {
+                const newLoadings = [...prevLoadings];
+                newLoadings[index] = false;
+                return newLoadings;
+            });
+        }, 6000);
+    };
+
+    return (
+        <Space direction="vertical">
+            <Space wrap>
+                <Button  type="primary" loading>
+                    Loading
+                </Button>
+                <Button type="primary" size="small" loading>
+                    Loading
+                </Button>
+                <Button type="primary" icon={<PoweroffOutlined />} loading />
+            </Space>
+
+            <Space wrap>
+                <Button type="primary" loading={loadings[0]} onClick={() => enterLoading(0)}>
+                    Click me!
+                </Button>
+                <Button
+                    type="primary"
+                    icon={<PoweroffOutlined />}
+                    loading={loadings[1]}
+                    onClick={() => enterLoading(1)}
+                >
+                    Click me!
+                </Button>
+                <Button
+                    type="primary"
+                    icon={<PoweroffOutlined />}
+                    loading={loadings[2]}
+                    onClick={() => enterLoading(2)}
+                />
+            </Space>
+        </Space>
+    );
 };
-
-const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-};
-
-type FieldType = {
-    username?: string;
-    password?: string;
-    remember?: string;
-};
-
-const App: React.FC = () => (
-    <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item<FieldType>
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-            <Input.Password />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
-        >
-            <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-                Submit
-            </Button>
-        </Form.Item>
-    </Form>
-);
 
 export default App;
